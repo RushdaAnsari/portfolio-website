fabf8198acce353fdaed32ed710775a8f9e76536
Rushda Ansari <iamrushdaa@gmail.com>
2023-09-25 20:14:45 +0530

    initial

---
+++ my-app/src/App.js
@@ -0,0 +1,18 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { NavBar } from "./components/NavBar";
+import './App.scss';
+
+function App() {
+  return (
+    
+    <div className="App">
+        <NavBar/>
+
+    </div>
+    
+  
+    
+  );
+}
+
+export default App;
