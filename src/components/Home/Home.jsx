68ac223dba40de79ac0ba20edfae4367715131a3
Rushda Ansari <iamrushdaa@gmail.com>
2023-10-01 07:32:08 +0530

    fixed h1

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -21,8 +21,8 @@ export const Home =() => {
             
             <div className="text-container lg:mx-[100px] sm:mx-[2px] py-28 sm:py-48 lg:py-56 lg:float-left sm:float-center">
             
-              <div className="text-left">
-                <h1 className="custom-text font-bold text-gray-600 text-4xl sm:text-6xl lg:text-[100px] lg:leading-[6rem]">
+              <div className="flex-col text-left">
+                <h1 className="custom-text font-bold text-gray-600 lg:text-[100px] lg:lh-[6rem] sm:text-[4rem]">
                   Web <br/> Developer
                 </h1>
                 {/* <h1 id="name" className="mt-1 font-bold tracking-tight text-gray-900 sm:text-6xl">
@@ -34,9 +34,9 @@ export const Home =() => {
                 <div className="mt-8 flex items-left">
                   <a
                     href="mailto:iamrushdaa@gmail.com"
-                    className="rounded-md btn btn-warning text-decoration-none px-3.5 py-2.5 text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md btn btn-sm btn-warning text-decoration-none px-3.5 py-2.5 font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
-                    Get in touch<span aria-hidden="true">→</span>
+                    Get in touch<span aria-hidden="true" className='text-sm'>→</span>
                   </a>
                 </div>
               </div>
